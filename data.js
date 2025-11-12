262eb153a4e145f905b691994c7ad0660f09a337
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-11-12 09:35:59 -0500

    start assesment

---
+++ data.js
@@ -0,0 +1,61 @@
+const collections = {
+    'bestsellers': {
+        'products': [
+            {
+                'title': 'outside vibes t-shirt sunshine',
+                'proce': '104.95',
+                'featured-image': 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                'images': [
+                    'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                    'https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1113'
+                ],
+                'tags': [],
+                'url': '',
+            },
+            {
+                'title': 'nike bottle outside vibes forest green',
+                'proce': '104.95',
+                'featured-image': 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880',
+                'images': [
+                    'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880',
+                    'https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880'
+                ],
+                'tags': [],
+                'url': '',
+            },
+            {
+                'title': 'rest in nature t-shirt charcoal',
+                'proce': '104.95',
+                'featured-image': 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                'images': [
+                    'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                    'https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1112'
+                ],
+                'tags': [],
+                'url': '',
+            },
+            {
+                'title': 'outside vibes cap forest green',
+                'proce': '104.95',
+                'featured-image': 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1539',
+                'images': [
+                    'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1539',
+                    'https://images.unsplash.com/photo-1605666807892-8c11d020bede?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1471'
+                ],
+                'tags': [],
+                'url': '',
+            },
+            {
+                'title': 'rest in nature t-shirt charcoal',
+                'proce': '104.95',
+                'featured-image': 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                'images': [
+                    'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470',
+                    'https://plus.unsplash.com/premium_photo-1686730540270-93f2c33351b6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1632'
+                ],
+                'tags': [],
+                'url': '',
+            }
+        ],
+    }
+}
